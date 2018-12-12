d04768aaddddc055055716609eee7c3852d1d0fe
Ciemna_noc <agarkowa@gmail.com>
2018-12-12 21:54:01 +0100

    add internal method getDataFromGithub

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Content from './components/Content';
 import Splashscreen from './components/splashscreen/Splashscreen';
+import github_data from '/Users/home/Documents/Programming/getrepo/src/api/Github'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.getDataFromGithub = this.getDataFromGithub.bind(this)
+  }
+  getDataFromGithub = () => {
+    console.log("GET DATA FROM GITHUB")
+  }
+  componentDidMount() {
+    this.getDataFromGithub();
+  }
   render() {
     return (
       <div className="Github repo">
---
+++ src/api/Github.js
@@ -0,0 +1,6 @@
+export default () => {
+  const githab_data = {
+      api : "https://api.github.com"
+  }
+  return githab_data;
+}
