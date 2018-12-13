be43e7b6c6eec387b031240d6a5c7a339b7fecd1
Ciemna_noc <agarkowa@gmail.com>
2018-12-13 01:16:09 +0100

    added some style to header of context component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,35 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <div className="header">
         <nav>
-          <logo style={{
-            paddingLeft : '10px',
-            paddingTope : '10px',
-            fontSize: '64px',
-            color: '244858'
-          }}>
-            <i className="fab fa-github-alt"></i>
-          </logo>
-          <ul className='menu'>
-            <li>Projects</li>
-            <li>Groups</li>
-            <li>Activity</li>
-            <li>Search</li>
-            <li>PullRequests</li>
-            <li>Issues</li>
+          
+          <ul className='menu' role="navigation">
+            <li>
+                <logo style={{
+                paddingLeft : '2px',
+                fontSize: '28px',
+                color: '#e0e0eb'
+              }}>
+                  <i className="fab fa-github-alt"></i>
+              </logo>
+
+            </li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Groups</a></li>
+            <li><a href="#">Activity</a></li>
+            <li className="search">
+              <span style={{
+                  fontSize:"20px",
+                  color: 'yellow',
+                  marginRight: '5px'
+                }}>
+                <i className="fas fa-search"></i>
+              </span>
+              <input type="text" placeholder="Search or jump to..."></input>
+            </li>
+            <li><a href='#'>PullRequests</a></li>
+            <li><a href="#">Issues</a></li>
           </ul>
 
         </nav>
