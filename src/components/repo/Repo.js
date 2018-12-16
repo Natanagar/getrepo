0b8c3f80babfaf6a417dd5c3ae875ecfcd9e5537
Ciemna_noc <agarkowa@gmail.com>
2018-12-16 03:57:29 +0100

    add static getDerived

--- src/components/repo/Repo.js
+++ src/components/repo/Repo.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Github from '/Users/home/Documents/Programming/getrepo/src/api/Github';
-import {withRouter } from "react-router" 
+import Counter from '/Users/home/Documents/Programming/getrepo/src/components/counter/Counter'
+import {withRouter } from "react-router";
+import moment from 'moment';
 
 class GithubList extends Component{
     state ={ 
-        listOfRepoFromGithub : []
+        listOfRepoFromGithub : [],
+        api : ""
     }
-    componentDidMount(){
+    getDataFromApi = () => {
         axios({
             method: 'get',
             url: Github.api
           })
           .then((response) => {
             const reposFromGithub = Array.from(response.data);
-            if(
-                reposFromGithub !== this.state.listOfRepoFromGithub
-            ){
+            if(reposFromGithub !== this.state.listOfRepoFromGithub){
                 this.setState({
                     listOfRepoFromGithub : reposFromGithub  
                 })
@@ -25,20 +26,77 @@ class GithubList extends Component{
           .catch(error => console.log(error))
         }
     
+    getInfoAboutRepo = () => {
+            axios({
+                method: 'get',
+                url: this.state.api 
+              })
+              .then((response) => {
+                const reposFromGithub = Array.from(response.data);
+                    this.setState({
+                        listOfRepoFromGithub : reposFromGithub  
+                    })
+                })
+              .catch(error => console.log(error))
+            }   
+    
+
+    
+    componentDidMount(){
+        this.getDataFromApi()
+    }
+    static getDerivedStateFromProps(props, state) {
+      const repoKey = props.match.url.substring(6)
+      const repo = state.listOfRepoFromGithub.filter(repo => repo.id == repoKey)
+      let api = String(repo.map(item => item.contents_url))
+      console.log(api)
+      state.api = api
+      //const repoForRender = this.state.listOfRepoFromGithub.filter(repo => repo.id == repoId)
+      return null;
+    }
+        
     render(){
             const { match, location, history } = this.props
-            const { listOfRepoFromGithub } = this.state
+            const { listOfRepoFromGithub, api } = this.state
+            console.log(api)
             const repoId = match.url.substring(6) 
             const repoForRender = listOfRepoFromGithub.filter(repo => repo.id == repoId)
-            console.log(Object.entries(repoForRender)[0]))
+            
         return(
             <div className="list-of-the-repos">
-                <ul>
-                    Hello World!
-                    {/*{repoForRender[0].map(repo=><li>
-                        {repo.name}
-                    </li>)}*/}
-                </ul>
+                <table>
+                    <thead>
+                        <tr>
+                            {repoForRender.map(item=> 
+                            <th style={{
+                                fontFamily: 'Noto Serif TC',
+                                textTransform : "uppercase",
+                                fontSize: '24px',
+                                fontWeight: '800'
+
+                            }}>
+                                {item.name}
+                            </th>)}
+                        </tr>
+                    </thead>
+                    <Counter />
+                    <tbody>
+                        {repoForRender.map(item=>
+                            <tr key={item.id}>
+                                <td style={{
+                                    fontSize: '18px'
+                                }}
+                                
+                                >{item.full_name}</td>
+                                <td style={{
+                                    fontStyle : 'italic'
+                                }}
+                                >Last updated {moment(item.updated_at.substr(0,10)).fromNow()}</td>
+                                <td>{item.language}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
         )
     }
